e39d2d20680402ed727ad21c3499ce00d404ba6d
unknown <tunh1991@gmail.com>
2019-04-23 22:58:58 +0700

    Add new file

--- app/src/main/java/com/example/extractapk/SettingActivity.java
+++ app/src/main/java/com/example/extractapk/SettingActivity.java
@@ -1,13 +1,13 @@
-package com.example.myapplication;
+package com.example.extractapk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_setting);
     }
 }
